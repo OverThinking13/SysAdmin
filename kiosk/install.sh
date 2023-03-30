b97adfd00b348b9fe77f25b4aff5ed47b3dea098
OverThinking13 <rexdiesel@mail.ru>
2023-03-30 18:51:40 +0800

    Create install.sh

---
+++ kiosk/install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+apt-get update
+
+apt install -y xorg openbox numlockx
+
+wget https://github.com/deemru/Chromium-Gost/releases/download/111.0.5563.147/chromium-gost-111.0.5563.147-linux-amd64.deb
+
+apt install -f -y ./*.deb
+
+useradd -m -g users kiosk
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty -a kiosk --noclear %I $TERM
+EOF
+
+cat >> /home/kiosk/.profile <<EOF
+if [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty1 ]; then
+. startx
+logout
+fi
+EOF
+
+cat > /home/kiosk/chromium-gost.sh <<EOF
+#!/bin/bash
+while true
+do
+/usr/bin/chromium-gost --disable-infobars \
+    --incognito\
+    --kiosk\
+    --no-first-run\
+    --disable\
+    --disable-translate\
+    --disable-infobars\
+    --disable-suggestions-service\
+    --disable-save-password-bubble\
+    --start-maximized\
+    --start-fullscreen\
+    --noerrdialogs\
+    --no-first-run\
+    --fast\
+    --fast-start \
+    --disable-infobars \
+    --disable-features=TranslateUI\
+    --disk-cache-dir=/dev/null\
+    --password-store=basic\
+    --disable-pinch \
+    --overscroll-history-navigation=disabled \
+    --disable-features=TouchpadOverscrollHistoryNavigation\
+    'https://www.gosuslugi.ru/'\
+    sleep 30s
+done
+EOF
+
+chmod +x /home/kiosk/chromium-gost.sh
+
+cat >> /etc/xdg/openbox/autostart <<EOF
+xset s off
+xset s noblank
+numlockx on
+./chromium-gost.sh
+EOF
+
+sed 's/<!-- Keybindings for desktop switching -->/\
+<!-- Keybindings for desktop switching -->\
+<keybind key="F12">\
+      <action name="Execute">\
+        <command>\/sbin\/shutdown -h now<\/command>\
+      <\/action>\
+    <\/keybind>\
+/' /etc/xdg/openbox/rc.xml > rc.xml
+
+mkdir -p /home/kiosk/.config/openbox/
+mv rc.xml /home/kiosk/.config/openbox/rc.xml
+
+chown -R kiosk:users /home/kiosk/{*,.*}
+
+sudo chmod u+s /sbin/shutdown
+
+sed  's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub > grub
+sudo mv grub /etc/default/grub
+
+update-grub
+
+rm *.deb
+
+rm install.sh
+
+reboot
